b4c5a6b1b2a388a4a9e467be320d37a0fb913876
Njhlx <helongxiang@foxmail.com>
2018-05-06 22:05:33 +0800

    更新

---
+++ 03_helloFunc2.py
@@ -0,0 +1,10 @@
+#定义参数
+def hello(name):
+    print('Hello ' + name + '!')
+
+#指定name
+hello('hlx')
+
+#输入name
+n = input()
+hello(n)
---
+++ 3_helloFunc.py
@@ -0,0 +1,8 @@
+#创建hello函数
+def hello():
+    print('Howdy!')
+    print('Howdy!!!')
+    print('Hello there.')
+
+
+hello()
