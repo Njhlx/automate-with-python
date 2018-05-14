9c30afb9f48dc988838028dbdda7483190aeab89
Njhlx <helongxiang@foxmail.com>
2018-05-14 22:32:39 +0800

    修改

--- 3_Collatz.py
+++ 3_Collatz.py
@@ -1,24 +1,21 @@
-#Collatz序列
+#Collatz序列，不论录入啥数字，最终结果都是1哦
 
 def collatz(number):
     if number % 2 == 0:
         number = number // 2
-        print(number)
+        return (number)
+#        print(number)
     elif number % 2 == 1:
         number = 3 * number + 1
-        print(number)
-
+        return (number)
+#        print(number)
 
 print('请输入一个整数')
 number = int(input())
-collatz(number)
-
-while number == 4:
-    x = collatz(number)
-    number = collatz(x)
-
-
-# while number != 1:
-#     number = collatz(number)
-#     collatz(number)
+x = collatz(number)
+while x != 1:
+    print(x)
+    x = collatz(x)
+else:
+    print(x)
 
