fc474826a9eb363908cf43f51f04f32909e4388d
Njhlx <helongxiang@foxmail.com>
2018-05-27 15:04:58 +0800

    新增

---
+++ 5_keys_values_items.py
@@ -0,0 +1,15 @@
+spam = {'color': 'red', 'age': 42}
+
+# for v in spam.values():
+#     print(v)
+#
+# for k in spam.keys():
+#     print(k)
+#
+# for i in spam.items():
+#     print(i)
+
+a = spam.keys()
+b = list(spam.keys())
+print(a)
+print(b)
---
+++ 5_myCat.py
@@ -0,0 +1,6 @@
+mycat = {'size':'fat','color':'gray','disposition':'loud'}
+
+x = mycat['size']
+print(x)
+
+print('My cat has ' + mycat['color'] + 'fur.')
